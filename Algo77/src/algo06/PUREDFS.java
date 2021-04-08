f159044cd7e9c8dc7b3343c91371602b159b4e11
1160300407 <290690260@qq.com>
2021-04-08 21:50:55 +0800

    debug

---
+++ Algo77/src/algo06/PUREDFS.java
@@ -0,0 +1,14 @@
+package algo06;
+
+import util.TreeNode;
+
+public class PUREDFS {
+    TreeNode root;
+    public PUREDFS(TreeNode u) {
+        root = u;
+    }
+
+    public void dfs() {
+
+    }
+}
